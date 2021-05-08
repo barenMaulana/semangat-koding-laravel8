24a93017c032f16182df01c7e4b16c617ec6b20b
baren maulana <barenmaulana@gmail.com>
2021-05-08 12:42:46 +0700

    add tutorial page

--- resources/views/tutorial-mentor.blade.php
+++ resources/views/tutorial-mentor.blade.php
@@ -3,25 +3,25 @@
             <div class="py-12 px-12">
                 <div class="flex flex-wrap justify-center">
                     <div class="course-video mx-5 my-5">
-                        <h3 class="font-bold text-2xl text-center">Judul</h3>
+                        <h3 class="font-bold text-2xl text-center">Menambah Kursus</h3>
                         <iframe width="320" height="215" class="border-8 border-blue-500 rounded-xl"
                         src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=0&mute=1">
                         </iframe> 
                     </div>
                     <div class="course-video mx-5 my-5">
-                        <h3 class="font-bold text-2xl text-center">Judul</h3>
+                        <h3 class="font-bold text-2xl text-center">Masukan Video Kursus</h3>
                         <iframe width="320" height="215" class="border-8 border-blue-500 rounded-xl"
                         src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=0&mute=1">
                         </iframe> 
                     </div>
                     <div class="course-video mx-5 my-5">
-                        <h3 class="font-bold text-2xl text-center">Judul</h3>
+                        <h3 class="font-bold text-2xl text-center">Meminta Diskon Kursus</h3>
                         <iframe width="320" height="215" class="border-8 border-blue-500 rounded-xl"
                         src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=0&mute=1">
                         </iframe> 
                     </div>
                     <div class="course-video mx-5 my-5">
-                        <h3 class="font-bold text-2xl text-center">Judul</h3>
+                        <h3 class="font-bold text-2xl text-center">Penarikan Saldo</h3>
                         <iframe width="320" height="215" class="border-8 border-blue-500 rounded-xl"
                         src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=0&mute=1">
                         </iframe> 
